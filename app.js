4d11127abe177baf5a074e4016e2e8cdca56c92f
David Wang <wqx1331@gmail.com>
2019-07-13 16:04:27 -0700

    add app.js

---
+++ app.js
@@ -0,0 +1,51 @@
+window.addEventListener('load', ()=>{
+  let long;
+  let lat;
+  let temperatureDescription=document.querySelector('.temperature-description')
+  let locationTimezone=document.querySelector(".location-timezone")
+  let temperatureSelection=document.querySelector(".temperature-section");
+  const temperatureSpan=document.querySelector('.temperature-section span');
+
+}
+
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position=>){
+        long=position.coords.longitude;
+        lat=position.coords.latitude;
+
+        const proxy="https://cors-anywhere.herokuapp.com/";
+
+        const api=`${proxy}
+        ${lat},${long};
+
+
+    fetch(api)
+      .then(response =>{
+          return response.json();
+      })
+      .then(data=> {
+          console.log(data);
+          const{temperature,summary}=data.currently;
+          temperatureDegree.textContent=temperature;
+          termperatureDescription.textContent=summary;
+          locationTimezone.textContent=data.timezone;
+          setIcons(icon,document.querySelector(".icon"));
+
+          temperatureSection.addEventListener('click',()=>{
+              if(temperatureSpan.textContent==="F"){
+                  temperatureSpan.textContent="C";
+              }else{
+                  temperatureSpan.textContent="F";
+              }
+          })
+      });
+    });
+}
+
+function setIcons(icon,iconID){
+    const skycons=new Skycons({color: "white"});
+    const currentIcon=icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
+}
+});
